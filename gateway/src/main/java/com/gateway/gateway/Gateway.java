81a472765198dd8c6ddc4cc913c696b6012867d7
Youngsik Yang <vacu9708@gmail.com>
2023-07-11 04:04:34 +0900

    Added docker-compose

--- gateway/src/main/java/com/gateway/gateway/Gateway.java
+++ gateway/src/main/java/com/gateway/gateway/Gateway.java
@@ -7,10 +7,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RestController
 public class Gateway {
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Replace this with specific origin URLs if needed
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+                        // .allowedHeaders("*")
+                        // .allowCredentials(false)
+                        // .maxAge(3600);
+            }
+        };
+    }
 
     @RequestMapping("/circuitbreaker_fallback")
 	public String circuitbreakerfallback() {
@@ -20,18 +36,17 @@ public class Gateway {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
-			// .route("order_management", r -> r
-			// 	.path("/order_management/**")
+			// .route("order_management", r -> r.path("/order_management/**")
 			// 	.filters(f -> f.rewritePath("/order_management/(?<path>.*)", "/${path}")) // Convert /order_management/abc to /abc
-			// 	.uri("http://localhost:8081")
+			// 	.uri("http://order_management:8083/")
 			// )
-			.route("order_management", r -> r.path("/order_management/**")
+			.route("user_management", r -> r.path("/user_management/**")
 				.uri("http://localhost:8081/")
 			)
-			.route("user_management", r -> r.path("/user_management/**")
+			.route("product_management", r -> r.path("/product_management/**")
 				.uri("http://localhost:8082/")
 			)
-			.route("product_management", r -> r.path("/product_management/**")
+			.route("order_management", r -> r.path("/order_management/**")
 				.uri("http://localhost:8083/")
 			)
 			.route("notification", r -> r.path("/notification/**")
--- order_management/src/main/java/com/order_management/order_management/order/OrderController.java
+++ order_management/src/main/java/com/order_management/order_management/order/OrderController.java
@@ -1,10 +1,12 @@
 package com.order_management.order_management.order;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @RequestMapping("/order_management")
@@ -14,4 +16,33 @@ public class OrderController {
         return "Received path parameter value: " + msg;
     }
 
+    @GetMapping("/internal_test")
+    String getInternalTest() {
+        CompletableFuture<String> promise1 = WebClient.create()
+            .get()
+            .uri("http://localhost:8080/circuitbreaker_fallback")
+            .retrieve()
+            .bodyToMono(String.class)
+            .toFuture();
+
+        String res1 = promise1.join();
+
+        System.out.println(res1);
+        return "OK";
+    }
+
+    @GetMapping("/external_test")
+    String getExternalTest() {
+        CompletableFuture<String> promise1 = WebClient.create()
+            .get()
+            .uri("https://www.google.com")
+            .retrieve()
+            .bodyToMono(String.class)
+            .toFuture();
+
+        String res1 = promise1.join();
+
+        System.out.println(res1);
+        return "OK";
+    }
 }
