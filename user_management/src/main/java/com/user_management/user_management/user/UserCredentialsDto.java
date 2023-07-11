9e01cfdfcebb2b09b5eb239b26148366073a4ca0
Youngsik Yang <vacu9708@gmail.com>
2023-07-12 04:53:56 +0900

    Added dependencies for user

--- user_management/src/main/java/com/user_management/user_management/user/UserCredentialsDto.java
+++ user_management/src/main/java/com/user_management/user_management/user/UserCredentialsDto.java
@@ -1,7 +1,8 @@
 package com.user_management.user_management.user;
 
+import java.io.Serializable;
+
 public class UserCredentialsDto implements Serializable  {
     private String username;
     private String password;
-    private String email;
 }
--- user_management/src/main/java/com/user_management/user_management/user/UserService.java
+++ user_management/src/main/java/com/user_management/user_management/user/UserService.java
@@ -1,5 +1,27 @@
 package com.user_management.user_management.user;
 
+import java.util.Map;
+
 public class UserService {
+
+    public boolean registerUser(UserCredentialsDto userCredentialsDto) {
+        return false;
+    }
+
+    public Map<String, String> login(UserCredentialsDto userCredentialsDto) {
+        return null;
+    }
+
+    public String verifyToken(UserCredentialsDto accessToken) {
+        return null;
+    }
+
+    public Map<String, String> reissueToken(String refreshToken) {
+        return null;
+    }
+
+    public boolean deleteUser(String accessToken) {
+        return false;
+    }
     
 }
