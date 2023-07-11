256c0f7e5ca20f1e64249a27b10fbb072639ee29
Youngsik Yang <vacu9708@gmail.com>
2023-07-11 16:58:11 +0900

    Added UserController and UserCredentialsDto

--- user_management/src/main/java/com/user_management/user_management/user/UserCredentialsDto.java
+++ user_management/src/main/java/com/user_management/user_management/user/UserCredentialsDto.java
@@ -1,5 +1,7 @@
 package com.user_management.user_management.user;
 
-public class UserCredentialsDto {
-    
+public class UserCredentialsDto implements Serializable  {
+    private String username;
+    private String password;
+    private String email;
 }
