09577d8642001a1a3d55e33073c2d0d71b4ae0d0
Youngsik Yang <vacu9708@gmail.com>
2023-08-18 21:17:11 +0900

    Configured Docker

--- Build all.bat
+++ Build all.bat
@@ -2,8 +2,19 @@ cd gateway
 mvnw clean package
 cd ..
 
-
 cd order_management
+mvnw clean package
+cd ..
+
+cd user_management
+mvnw clean package
+cd ..
+
+cd product_management
+mvnw clean package
+cd ..
+
+cd notification
 mvnw clean package
 cd ..
 
